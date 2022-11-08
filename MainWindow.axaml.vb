4fd8527471f519e95c838cde0e5ef784cb5e52e6
Maurits van der Schee <maurits@vdschee.nl>
2022-11-08 20:11:09 +0100

    initial commit

---
+++ MainWindow.axaml.vb
@@ -0,0 +1,9 @@
+Imports Avalonia.Controls
+
+Public Partial Class MainWindow
+    Inherits Window
+
+    Public Sub New()
+        'InitializeComponent()
+    End Sub
+End Class
---
+++ Program.vb
@@ -0,0 +1,13 @@
+﻿Imports Avalonia
+Imports System
+
+Class Program
+
+    Public Shared Sub Main(ByVal args As String())
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args)
+    End Sub
+
+    Public Shared Function BuildAvaloniaApp() As AppBuilder
+        Return AppBuilder.Configure(Of App)().UsePlatformDetect().LogToTrace()
+    End Function
+End Class
